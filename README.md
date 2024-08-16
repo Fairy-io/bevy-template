1c4cb52b137ef867f7fb7c95025ff13232a27b91
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-08-16 18:00:42 +0200

    adding hot reloading

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # bevy-template
+
+`cargo run --features "dev"`
+`cargo watch -w lib -x "build -p lib"`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
 use bevy_template::plugins::base_plugin::BasePlugin;
 
+#[cfg(not(feature = "dev"))]
+use lib::*;
+
+#[cfg(feature = "dev")]
+#[hot_lib_reloader::hot_module(dylib = "lib")]
+mod lib_hot {
+    use bevy::prelude::*;
+
+    hot_functions_from_file!("lib/src/lib.rs");
+}
+
+#[cfg(feature = "dev")]
+use lib_hot::*;
+
 fn main() {
     App::default()
         .add_plugins(DefaultPlugins)
         .add_plugins(BasePlugin)
+        .add_systems(Update, startup)
         .run();
 }
