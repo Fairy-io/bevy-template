5d10d20ec93e5c3fb794cb78e8ccd4cce2d0867e
massfice <adrian.marian.tomasz.larysz@gmail.com>
2024-08-17 20:27:16 +0200

    adding basic test

--- lib/src/systems/startup_system.rs
+++ lib/src/systems/startup_system.rs
@@ -9,3 +9,23 @@ pub fn startup(mut commands: Commands) {
         Despawnable {},
     ));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_if_text_exists() {
+        let mut app = App::default();
+
+        app.add_systems(Startup, startup);
+
+        app.update();
+
+        let world = app.world_mut();
+
+        let text = world.query::<&Text>().iter(world).next().unwrap();
+
+        assert_eq!(text.sections[0].value, "Base plugin");
+    }
+}
